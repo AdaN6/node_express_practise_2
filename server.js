a43ec76300fc3c32c4b26c0f02e6e5795bc5732c
AdaN6 <a_disney_da@hotmail.com>
2022-11-18 13:31:46 +0100

    added views

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-console.log('Here')
-res.send('Hi')
+console.log('now')
+// res.sendStatus(500)
+// res.status(500).send("hi")
+// res.status(500).json({message: "Error"});
+// res.json({ message: "Error" });
+// res.download("server.js");
+// res.render('index')
+res.render("index", { text: 'World' });
+
+
 })
 
 app.listen(3000)
