95e18e6be691edab9fbdc2687d93d32441006add
AdaN6 <a_disney_da@hotmail.com>
2022-11-18 13:53:52 +0100

    implied route

--- routes/users.js
+++ routes/users.js
@@ -9,5 +9,19 @@ router.get("/new", (req, res) => {
   res.send("User New Form");
 });
 
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`Get User with ID ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Put User with ID ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete User with ID ${req.params.id}`);
+  })
+  
+
+
 module.exports = router
 
